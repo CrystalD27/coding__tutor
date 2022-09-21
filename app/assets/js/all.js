e5efd7350ccae513cee1b57fb1dd5db4e14c3571
Crystal <ytgliniak@gmail.com>
2022-09-21 22:20:59 +0200

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,35 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".commentSwiper", {
+
+  slidesPerView: 1,
+  // 倒底會回第一張
+  loop: true,
+  // 有fade效果才不會整張切換
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
+const elem = document.querySelector('input[name="datepicker"]');
+const datepicker = new Datepicker(elem, {
+
+});
+
+
+
+
